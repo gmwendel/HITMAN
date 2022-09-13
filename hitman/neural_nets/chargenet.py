6c702fe85ed1a7fb884a769bd4c96d2c9992fdc5
gmwendel <28656471+gmwendel@users.noreply.github.com>
2022-09-13 17:58:00 -0400

    Change activation for chargenet layers to mish

--- hitman/neural_nets/chargenet.py
+++ hitman/neural_nets/chargenet.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-
+import tensorflow_addons as tfa
 from hitman.neural_nets.transformations import chargenet_trafo
 
-def get_chargenet(labels):
+def get_chargenet(labels,activation=tfa.activations.mish):
 
     charge_input = tf.keras.Input(shape=(2,))
     params_input = tf.keras.Input(shape=(len(labels),))
@@ -10,21 +10,25 @@ def get_chargenet(labels):
     t = chargenet_trafo(labels=labels)
 
     h = t(charge_input, params_input)
-    h = tf.keras.layers.Dense(32, activation="relu")(h)
+    h = tf.keras.layers.Dense(32, activation=activation)(h)
     h = tf.keras.layers.Dropout(0.001)(h)
-    h = tf.keras.layers.Dense(64, activation="relu")(h)
+    h = tf.keras.layers.Dense(64, activation=activation)(h)
     h = tf.keras.layers.Dropout(0.001)(h)
-    h = tf.keras.layers.Dense(128, activation="relu")(h)
+    h = tf.keras.layers.Dense(128, activation=activation)(h)
     h = tf.keras.layers.Dropout(0.001)(h)
-#    h = tf.keras.layers.Dense(256, activation="relu")(h)
+    h = tf.keras.layers.Dense(256, activation=activation)(h)
+    h = tf.keras.layers.Dropout(0.001)(h)
+#    h = tf.keras.layers.Dense(512, activation=activation)(h)
 #    h = tf.keras.layers.Dropout(0.001)(h)
-#    h = tf.keras.layers.Dense(256, activation="relu")(h)
+#    h = tf.keras.layers.Dense(512, activation=activation)(h)
 #    h = tf.keras.layers.Dropout(0.001)(h)
-    h = tf.keras.layers.Dense(128, activation="relu")(h)
+    h = tf.keras.layers.Dense(256, activation=activation)(h)
     h = tf.keras.layers.Dropout(0.001)(h)
-    h = tf.keras.layers.Dense(64, activation="relu")(h)
+    h = tf.keras.layers.Dense(128, activation=activation)(h)
     h = tf.keras.layers.Dropout(0.001)(h)
-    h = tf.keras.layers.Dense(32, activation="relu")(h)
+    h = tf.keras.layers.Dense(64, activation=activation)(h)
+    h = tf.keras.layers.Dropout(0.001)(h)
+    h = tf.keras.layers.Dense(32, activation=activation)(h)
     h = tf.keras.layers.Dropout(0.001)(h)
     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(h)
 
