dd0ee455d672afa408aa5b5e1763961b76113e31
gmwendel <28656471+gmwendel@users.noreply.github.com>
2023-01-09 15:11:09 -0500

    Add a bandaid to simulation producing several keys for the output. E.g. ['output;3', 'output;2', 'meta;1']

--- hitman/tools/ratextract.py
+++ hitman/tools/ratextract.py
@@ -6,16 +6,31 @@ class DataExtractor():
     def __init__(self, input_files):
         # Validate all files are valid, do not load invalid files
         valid_files = []
+        valid_out_keys = []
         for infile in input_files:
             if self.file_isvalid(infile):
                 valid_files.append(infile)
+                valid_out_keys.append(self.get_valid_out_key(infile))  # This is a temporary hack
+
+        self.out_keys = valid_out_keys
         self.input_files = valid_files
+
+    # Need to define a better way to get the valid outkey if there are several output trees in file
+    def get_valid_out_key(self, infile):
+        with uproot.open(infile) as file:
+            return file.keys()[0]
 
     def file_isvalid(self, infile):
         try:
-            with uproot.open(infile + ":output;1") as file:
-                pass
-            return True
+            with uproot.open(infile) as file:
+                outputTree = file['output']
+                print(file.keys())
+                if len(file.keys()) > 1:
+                    return True
+                else:
+                    print("Warning: Opening " + infile + " failed")
+                    return False
+
         except:
             print("Warning: Opening " + infile + " failed")
             return False
@@ -25,8 +40,9 @@ class DataExtractor():
         return hyp_norm, obs_norm
 
     def get_hitman_train_data(self):
-        obsdata = uproot.concatenate([infile + ":output;1" for infile in self.input_files],
-                                     filter_name=["hitPMTID", "hitPMTTime", "hitPMTCharge"], library='np')
+        obsdata = uproot.concatenate(
+            [self.input_files[i] + ":" + self.out_keys[i] for i in range(len(self.input_files))],
+            filter_name=["hitPMTID", "hitPMTTime", "hitPMTCharge"], library='np')
         maps = uproot.concatenate([infile + ":meta;1" for infile in self.input_files],
                                   filter_name=["pmtX", "pmtY", "pmtZ"], library='np')
 
@@ -35,7 +51,7 @@ class DataExtractor():
         charge_obs = np.stack([
             np.array([np.sum(charge) for charge in obsdata['hitPMTCharge']], dtype=np.float32),
             nhit.astype(np.float32)
-        ],axis=1
+        ], axis=1
         )
         hit_obs = np.stack([
             maps['pmtX'][0][idx].astype(np.float32),
@@ -48,8 +64,9 @@ class DataExtractor():
 
         del obsdata
 
-        hypdata = uproot.concatenate([infile + ":output;1" for infile in self.input_files],
-                                     filter_name=['mcx', 'mcy', 'mcz', 'mcu', 'mcv', 'mcw', 'mcke'], library='np')
+        hypdata = uproot.concatenate(
+            [self.input_files[i] + ":" + self.out_keys[i] for i in range(len(self.input_files))],
+            filter_name=['mcx', 'mcy', 'mcz', 'mcu', 'mcv', 'mcw', 'mcke'], library='np')
         mcaz = np.mod(np.arctan2(hypdata['mcv'], hypdata['mcu']), 2 * np.pi).astype(np.float32)
         mcze = np.arccos(hypdata['mcw'] / np.linalg.norm([hypdata['mcu'], hypdata['mcv'], hypdata['mcw']], 2)).astype(
             np.float32)
