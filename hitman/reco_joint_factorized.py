30c78a9262da1be595094a175016d3e131e199a2
gmwendel <28656471+gmwendel@users.noreply.github.com>
2026-04-14 16:27:35 -0400

    fix(reco): add fixed_eps and b_val support for LLH evaluation

--- hitman/reco_joint_factorized.py
+++ hitman/reco_joint_factorized.py
@@ -56,7 +56,7 @@ def newton_raphson_epsilon(S, Lambda_sig, b, charges, max_iter=20, tol=1e-5):
     return eps
 
 @tf.function
-def tfLLH_joint_factorized(charges, pmt_positions, hyp_batch, yields, shape_net, acc_net, b_val=0.1):
+def tfLLH_joint_factorized(charges, pmt_positions, hyp_batch, yields, shape_net, acc_net, b_val=0.1, fixed_eps=None):
     """
     Calculates the unbinned extended log-likelihood over a stacked batch of events.
     """
@@ -64,12 +64,7 @@ def tfLLH_joint_factorized(charges, pmt_positions, hyp_batch, yields, shape_net,
     N_events = tf.shape(charges)[0]
     N_sensors = tf.shape(charges)[1]
     
-    # hyp_batch is (N_hyp, 3) where params are E, Scat, Abs
-    # Note: For our new factorized network, the energy is technically decoupled,
-    # but we still pass it inside the hypothesis array and use `yields` for absolute scale.
-    
     # We need to broadcast the hypothesis and PMT positions for the networks
-    # Since ShapeNet takes (N_hyp, N_sensors, 3) for PMTs
     pmt_broadcast = tf.expand_dims(pmt_positions, axis=0) # (1, N_sensors, 3)
     pmt_tiled = tf.tile(pmt_broadcast, [N_hyp, 1, 1]) # (N_hyp, N_sensors, 3)
     
@@ -80,21 +75,16 @@ def tfLLH_joint_factorized(charges, pmt_positions, hyp_batch, yields, shape_net,
     mu_nn = acc_net([hyp_batch])
     
     # Expand shapes to accommodate N_events
-    # f_nn -> (N_hyp, 1, N_sensors) -> (N_hyp, N_events, N_sensors)
     f_nn_exp = tf.expand_dims(f_nn, axis=1)
     f_nn_events = tf.tile(f_nn_exp, [1, N_events, 1])
     
-    # mu_nn -> (N_hyp, 1) -> (N_hyp, N_events)
     mu_nn_events = tf.tile(mu_nn, [1, N_events])
     
-    # yields: (N_events,) -> (1, N_events)
     yields_exp = tf.expand_dims(yields, axis=0)
     
-    # S_{i,j} = Y_j * mu * f_i
     # Y_j * mu: (N_hyp, N_events)
     Lambda_sig = yields_exp * mu_nn_events
     
-    # Lambda_sig_exp: (N_hyp, N_events, 1)
     Lambda_sig_exp = tf.expand_dims(Lambda_sig, axis=-1)
     
     # S = Lambda_sig_exp * f_nn_events -> (N_hyp, N_events, N_sensors)
@@ -103,21 +93,17 @@ def tfLLH_joint_factorized(charges, pmt_positions, hyp_batch, yields, shape_net,
     b = tf.ones((N_sensors,), dtype=tf.float32) * b_val
     
     # --- Step A: Profile Epsilon ---
-    eps = newton_raphson_epsilon(S, Lambda_sig, b, charges)
-    # eps shape: (N_hyp, 1)
-    
-    # --- Step B: Vertex Optimization (TODO) ---
-    # TODO: Inject parallel vertex maximization here. 
-    # For now, we bypass it since we assume fixed vertices in the grid search.
-    
+    if fixed_eps is None:
+        eps = newton_raphson_epsilon(S, Lambda_sig, b, charges)
+    else:
+        eps = tf.ones((N_hyp, 1), dtype=tf.float32) * fixed_eps
+        
     # Evaluate LLH
-    # lam_{i,j} = eps * S_{i,j} + b_i
     eps_broadcast = tf.expand_dims(eps, axis=-1) # (N_hyp, 1, 1)
     b_broadcast = tf.reshape(b, (1, 1, N_sensors))
     
     lam = eps_broadcast * S + b_broadcast
     
-    # LLH = sum( k * ln(lam) - lam )
     charges_broadcast = tf.expand_dims(charges, axis=0) # (1, N_events, N_sensors)
     
     llh_matrix = charges_broadcast * tf.math.log(lam) - lam
@@ -125,5 +111,4 @@ def tfLLH_joint_factorized(charges, pmt_positions, hyp_batch, yields, shape_net,
     # Sum over events and sensors to get the total log-likelihood per hypothesis
     llhs = tf.reduce_sum(llh_matrix, axis=[1, 2])
     
-    # We return the negative log-likelihood (NLL) for minimization
     return -llhs
